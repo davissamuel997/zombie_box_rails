419d10732d8f23e9106bb3b57789fbf7d4addee9
SamuelDavis <sam9@cfu.net>
2015-09-18 20:13:58 -0400

    paginating post data

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,45 @@ class Post < ActiveRecord::Base
   def self.get_posts(options = {})
     data = {:errors => false}
 
-    p 'hello world'
+    page_num = (options[:page] || 1).to_i
+    per_page = 12
+
+    posts = Post.all.page(page_num).per(per_page).order('created_at DESC')
+
+    data[:posts] = posts.map { |post| {
+        post_id:     post.id,
+        title:       post.title,
+        post_date:   post.post_date.present? ? post.post_date.strftime('%m/%d/%Y') : nil,
+        text:        post.text,
+        user:        post.user
+      }
+    }
+    
+    data[:pagination] = Post.pagination_data Post.all.count, page_num, per_page
 
     data
+  end
+
+  # This paginates all of the data for the response of the js.
+  def self.pagination_data element_count, current_page, results_per_page
+    page  = current_page.to_i
+    pages = (element_count / results_per_page.to_f).ceil
+
+    if pages <= 1
+      pages = 1
+      relevant_pages = []
+    else
+      # If possible, add the two previous and two next pages to the 'relevant pages' array
+      previous_pages = page - 2 < 1 ? (1..page).to_a : ( (page - 2)..page ).to_a
+      next_pages     = page + 2 > pages ? ( (page+1)..pages ).to_a : ( (page+1)..(page + 2) ).to_a
+      relevant_pages = previous_pages + next_pages
+
+      # Add the first and last page to the 'relevant pages' array if they are not present
+      relevant_pages.unshift(1) unless relevant_pages.first == 1
+      relevant_pages << pages   unless relevant_pages.last == pages
+    end
+
+    { total_items: element_count, pages: pages, relevant_pages: relevant_pages }
   end
 
 end
