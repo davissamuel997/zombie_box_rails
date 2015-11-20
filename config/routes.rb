790a7469e6b99caaa31e869777769cf2aeb9663a
SamuelDavis <sam9@cfu.net>
2015-11-20 12:37:26 -0500

    added test route

--- config/routes.rb
+++ config/routes.rb
@@ -69,6 +69,8 @@ Rails.application.routes.draw do
   get 'get_messages' => 'messages#get_messages'
   post 'safe_destroy' => 'messages#safe_destroy', as: 'message_safe_destroy'
 
+  get 'test_route' => 'users#test_route'
+
   root :to => 'users#welcome'
 
 end
