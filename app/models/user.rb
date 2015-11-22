446f0f32a1152562e68f7e506106d8701c1871a5
SamuelDavis <sam9@cfu.net>
2015-11-22 14:51:58 -0500

    matching the unity request

--- app/models/user.rb
+++ app/models/user.rb
@@ -202,8 +202,8 @@ class User < ActiveRecord::Base
     p options
     p JSON.parse(options)
 
-    if options[:user_params].present?
-      user_params = JSON.parse(options[:user_params])
+    if options.present? && options.first.present? && options.first.first.present?
+      user_params = JSON.parse(options.first.first)["user_params"]
 
       if user_params["user_id"].present? && user_params["user_id"].to_i > 0
 
@@ -215,37 +215,37 @@ class User < ActiveRecord::Base
 
           if user.update(total_points: total_points, total_kills: total_kills)
           
-            if user_params["weapons"].present? && user_params["weapons"].count > 0
-              user_params["weapons"].each do |weapon_hash|
-                if weapon_hash["weapon_id"].present? && weapon_hash["weapon_id"].to_i > 0
-                  weapon = Weapon.find(weapon_hash["weapon_id"])
+            # if user_params["weapons"].present? && user_params["weapons"].count > 0
+            #   user_params["weapons"].each do |weapon_hash|
+            #     if weapon_hash["weapon_id"].present? && weapon_hash["weapon_id"].to_i > 0
+            #       weapon = Weapon.find(weapon_hash["weapon_id"])
 
-                  kill_count = weapon_hash["kill_count"].present? && weapon_hash["kill_count"].to_i > 0 ? weapon_hash["kill_count"].to_i : weapon.try(:kill_count)
-                  damage     = weapon_hash["damage"].present? && weapon_hash["damage"].to_i > 0 ? weapon_hash["damage"].to_i : weapon.try(:damage)
-                  ammo       = weapon_hash["ammo"].present? && weapon_hash["ammo"].to_i > 0 ? weapon_hash["ammo"].to_i : weapon.try(:ammo)
-                  fire_rate  = weapon_hash["fire_rate"].present? && weapon_hash["fire_rate"].to_f >= 0 ? weapon_hash["fire_rate"].to_f : weapon.try(:fire_rate)
+            #       kill_count = weapon_hash["kill_count"].present? && weapon_hash["kill_count"].to_i > 0 ? weapon_hash["kill_count"].to_i : weapon.try(:kill_count)
+            #       damage     = weapon_hash["damage"].present? && weapon_hash["damage"].to_i > 0 ? weapon_hash["damage"].to_i : weapon.try(:damage)
+            #       ammo       = weapon_hash["ammo"].present? && weapon_hash["ammo"].to_i > 0 ? weapon_hash["ammo"].to_i : weapon.try(:ammo)
+            #       fire_rate  = weapon_hash["fire_rate"].present? && weapon_hash["fire_rate"].to_f >= 0 ? weapon_hash["fire_rate"].to_f : weapon.try(:fire_rate)
 
-                  unless weapon.present? && weapon.is_a?(Weapon) && weapon.update(kill_count: kill_count, damage: damage,
-                                                                                  ammo: ammo, fire_rate: fire_rate)
-                    data[:errors] = true
-                  end
-                end
-              end
-            end
+            #       unless weapon.present? && weapon.is_a?(Weapon) && weapon.update(kill_count: kill_count, damage: damage,
+            #                                                                       ammo: ammo, fire_rate: fire_rate)
+            #         data[:errors] = true
+            #       end
+            #     end
+            #   end
+            # end
 
-            if user_params["skins"].present? && user_params["skins"].count > 0
-              user_params["skins"].each do |skin_hash|
-                if skin_hash["skin_id"].present? && skin_hash["skin_id"].to_i > 0
-                  skin = Skin.find(skin_hash["skin_id"])
+            # if user_params["skins"].present? && user_params["skins"].count > 0
+            #   user_params["skins"].each do |skin_hash|
+            #     if skin_hash["skin_id"].present? && skin_hash["skin_id"].to_i > 0
+            #       skin = Skin.find(skin_hash["skin_id"])
 
-                  kill_count = skin_hash["kill_count"].present? && skin_hash["kill_count"].to_i > 0 ? skin_hash["kill_count"].to_i : skin.try(:kill_count)
+            #       kill_count = skin_hash["kill_count"].present? && skin_hash["kill_count"].to_i > 0 ? skin_hash["kill_count"].to_i : skin.try(:kill_count)
 
-                  unless skin.present? && skin.is_a?(Skin) && skin.update!(kill_count: kill_count)
-                    data[:errors] = true
-                  end
-                end
-              end
-            end
+            #       unless skin.present? && skin.is_a?(Skin) && skin.update!(kill_count: kill_count)
+            #         data[:errors] = true
+            #       end
+            #     end
+            #   end
+            # end
 
             if data[:errors] == false
               data[:user] = user.get_params
