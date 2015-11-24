4efd3720bb01224c54716273c3350c27062a2576
SamuelDavis <sam9@cfu.net>
2015-11-24 14:52:04 -0500

    added validations and fixed bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ActiveRecord::Base
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
+  validates :password, length: { minimum: 8 }
   validates_presence_of :email
   validates_uniqueness_of :email
 
